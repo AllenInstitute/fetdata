bf0ff5595315ea3a03abb319997926d828e3a6dc
edelsonc <charlese@alleninstitute.org>
2017-07-06 13:51:04 -0700

    Added documentation

---
+++ R/data.R
@@ -0,0 +1,93 @@
+#'Micro RNA Expression Values for the Fetal Human Brain Atlas.
+#'
+#'A dataset containing the normalized expression values for a single brain in the fetal human brain atlas.
+#'
+#'
+#'@format A data frame with 9540552 rows and 4 variables:
+#'\describe{
+#'  \item{donorID}{brain ID from the Allen Institute}
+#'  \item{gene}{gene the above probe corresponds to; gene to probe is many to one}
+#'  \item{brain_structure}{brain structure acronym}
+#'  \item{value}{normalized expression value}
+#'}
+#'@source \url{"http://www.brainspan.org/static/download.html"}
+#'
+"datFET.donor1"
+
+#'Micro RNA Expression Values for the Fetal Human Brain Atlas.
+#'
+#'A dataset containing the normalized expression values for a single brain in the fetal human brain atlas.
+#'
+#'
+#'@format A data frame with 9919840 rows and 4 variables:
+#'\describe{
+#'  \item{donorID}{brain ID from the Allen Institute}
+#'  \item{gene}{gene the above probe corresponds to; gene to probe is many to one}
+#'  \item{brain_structure}{brain structure acronym}
+#'  \item{value}{normalized expression value}
+#'}
+#'@source \url{"http://www.brainspan.org/static/download.html"}
+#'
+"datFET.donor2"
+
+#'Micro RNA Expression Values for the Fetal Human Brain Atlas.
+#'
+#'A dataset containing the normalized expression values for a single brain in the fetal human brain atlas.
+#'
+#'
+#'@format A data frame with 9044560 rows and 4 variables:
+#'\describe{
+#'  \item{donorID}{brain ID from the Allen Institute}
+#'  \item{gene}{gene the above probe corresponds to; gene to probe is many to one}
+#'  \item{brain_structure}{brain structure acronym}
+#'  \item{value}{normalized expression value}
+#'}
+#'@source \url{"http://www.brainspan.org/static/download.html"}
+#'
+"datFET.donor3"
+
+#'Micro RNA Expression Values for the Fetal Human Brain Atlas.
+#'
+#'A dataset containing the normalized expression values for a single brain in the fetal human brain atlas.
+#'
+#'
+#'@format A data frame with 6593776 rows and 4 variables:
+#'\describe{
+#'  \item{donorID}{brain ID from the Allen Institute}
+#'  \item{gene}{gene the above probe corresponds to; gene to probe is many to one}
+#'  \item{brain_structure}{brain structure acronym}
+#'  \item{value}{normalized expression value}
+#'}
+#'@source \url{"http://www.brainspan.org/static/download.html"}
+#'
+"datFET.donor4"
+
+#'Donor ID's for Brains in the Fetal Human Brain Atlas
+#'
+#'A vector of the donor ID's from the 4 brains in the fetal human brain atlas
+#'
+#'@format A character vector of length 4:
+#'
+#'@source \url{"http://www.brainspan.org/static/download.html"}
+#'
+"donorsFET"
+
+#'Genes for Brains in the Human Brain Atlas
+#'
+#'A vector of the genes measured from the 4 brains in the fetal human brain atlas
+#'
+#'@format A character vector of length 29176:
+#'
+#'@source \url{"http://www.brainspan.org/static/download.html"}
+#'
+"genesFET"
+
+#'Brain structures measured in the Fetal Human Brain
+#'
+#'A vector of the brain structure acronyms measured in the fetal human brain atlas
+#'
+#'@format A character vector of length 516:
+#'
+#'@source \url{"http://www.brainspan.org/static/download.html"}
+#'
+"structuresFET"
